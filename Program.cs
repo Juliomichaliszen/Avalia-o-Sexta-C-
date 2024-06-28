e6d75ae5ab8fc4754dcfd9fdb3fd432b20b7c5e9
Juliomichaliszen <163341158+Juliomichaliszen@users.noreply.github.com>
2024-06-28 10:19:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Prova A1");
+
+//ENDPOINTS DE CATEGORIA
+//GET: http://localhost:5273/categoria/listar
+app.MapGet("/categoria/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Categorias.Any())
+    {
+        return Results.Ok(ctx.Categorias.ToList());
+    }
+    return Results.NotFound("Nenhuma categoria encontrada");
+});
+
+//POST: http://localhost:5273/categoria/cadastrar
+app.MapPost("/categoria/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Categoria categoria) =>
+{
+    ctx.Categorias.Add(categoria);
+    ctx.SaveChanges();
+    return Results.Created("", categoria);
+});
+
+//ENDPOINTS DE TAREFA
+//GET: http://localhost:5273/tarefas/listar
+app.MapGet("/tarefas/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(ctx.Tarefas.ToList());
+    }
+    return Results.NotFound("Nenhuma tarefa encontrada");
+});
+
+//POST: http://localhost:5273/tarefas/cadastrar
+app.MapPost("/tarefas/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Tarefa tarefa) =>
+{
+    Categoria? categoria = ctx.Categorias.Find(tarefa.CategoriaId);
+    if (categoria == null)
+    {
+        return Results.NotFound("Categoria não encontrada");
+    }
+    tarefa.Categoria = categoria;
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+});
+
+//PUT: http://localhost:5273/tarefas/alterar/{id}
+app.MapPut("/tarefas/alterar{id}", async (int id, Tarefa inputTodo, DbContext db) =>
+{
+    var tarefa = await db.FindAsync(TarefaId);
+    if (t == null) return Results.NotFound();
+
+    Tarefa.status = inputTodo.TarefaId;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+//GET: http://localhost:5273/tarefas/naoconcluidas
+app.MapGet("/tarefas/naoconcluidas", ([FromServices] AppDataContext ctx) =>
+{
+     Tarefa? tarefa = ctx.Tarefas.Find(tarefa.Status);
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok("Categoria não encontrada");
+    }
+    tarefa.Categoria = null ;
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+    //Implementar a listagem de tarefas não concluídas
+});
+
+//GET: http://localhost:5273/tarefas/concluidas
+app.MapGet("/tarefas/concluidas", ([FromServices] AppDataContext ctx) =>
+{
+    //Implementar a listagem de tarefas concluídas
+});
+
+
+
+app.Run();
